69ac78a0ad80e1933e0adee4a90159fc70273e6a
cassmatheuss <cassmatheuss@gmail.com>
2025-07-04 18:02:00 -0300

    feat: initial files

---
+++ src/main/java/com/pi/cardapiofacil/CardapiofacilApplication.java
@@ -0,0 +1,13 @@
+package com.pi.cardapiofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiofacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/cardapiofacil/CardapiofacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.cardapiofacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapiofacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
